883800ffb32b932c944a076d307d7d933e094044
Daniel Rossi <danielrossi5678@gmail.com>
2022-06-06 18:45:13 -0300

    não to conseguindo iniciar o index no express, to ficando insano ja

---
+++ script.js
@@ -0,0 +1,159 @@
+'use strict'
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
+
+
+const openModal = () => document.getElementById('modal').classList.add('active');
+const closeModal = () => {
+    clearFields();
+    document.getElementById('modal').classList.remove('active');
+}
+
+//Variaveis do CRUD
+
+const getLocalStorage = ()=> JSON.parse(localStorage.getItem('db_client')) ??/*se for nulo*/ []
+const setLocalStorage = (dbClient)=> localStorage.setItem("db_client", JSON.stringify(dbClient))
+//CRUD
+
+//Create
+const createClient = (client) =>{
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
+
+
+//Read
+const readClient = () => getLocalStorage()
+
+
+//Update
+const updateClient = (index, client) =>{
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+
+//Delete
+const del = (index) =>{
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+
+
+//Ler o form
+const clearFields = () =>{
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
+
+const isValidFields = () =>{
+    return document.getElementById('form').reportValidity()
+}
+
+const saveClient = () =>{
+    if(isValidFields()){
+        const client ={
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value,
+        }
+        const index = document.getElementById('nome').dataset.index
+        if(index == 'new'){
+            createClient(client)
+            updateTable()
+            closeModal() 
+        }else{
+            updateClient(index, client)
+            updateTable()
+            closeModal()
+        }
+    }
+}
+
+
+//Colocar na tabela
+const clearTable = () =>{
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const createRow = (client, index) =>{
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green" data-action="edit-${index}">Editar</button>
+            <button type="button" class="button red" data-action="delete-${index}">Excluir</button>
+        </td>
+    `
+    document.querySelector('#tableClient>tbody').appendChild(newRow);
+}
+
+const updateTable = () =>{    
+    const dbClient = readClient()
+    clearTable()
+    dbClient.forEach(createRow)
+}
+updateTable()
+
+
+//Editar
+const editClient = (index) =>{
+    const client = readClient()[index]
+    client.index = index
+    fillFields(client)
+    openModal()
+}
+
+const fillFields = (client) =>{
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index
+}
+
+const editDelete = (event) =>{
+    if(event.target.type == 'button'){
+        const [action, index] = event.target.dataset.action.split('-'); 
+        
+        if(action == 'edit'){
+            editClient(index)
+        }else{
+            const client = readClient()[index]
+            const response = confirm(`Deseja excluir o(a) cliente ${client.nome}`)
+            if(response){
+                del(index)
+                updateTable()
+            }
+        }
+    }
+}
+
+
+
+//Eventos
+document.getElementById('cadastrarCliente').addEventListener('click', openModal);
+document.getElementById('modalClose').addEventListener('click', closeModal);
+
+document.getElementById('salvar').addEventListener('click', saveClient);
+
+document.querySelector('#tableClient>tbody').addEventListener('click', editDelete);
+
+//view
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "index.html")
+})
+
+app.listen(8080)
